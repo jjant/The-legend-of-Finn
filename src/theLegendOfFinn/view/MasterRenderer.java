1a5fc5ece5b46205b81f0d7f836351d3bcec0760
LCD <LCD>
2016-06-17 22:54:34 -0300

    Game finally done, everything is perfect.

--- src/theLegendOfFinn/view/MasterRenderer.java
+++ src/theLegendOfFinn/view/MasterRenderer.java
@@ -8,7 +8,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
